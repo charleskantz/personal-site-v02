6a01b2fbb44633b49078eaf6b150302bc45e5c54
Charles Kantz <kantz.charles@gmail.com>
2020-10-27 15:48:23 -0700

    more optimization: added React.lazy load for offscreen components, removed FA import (may be buggy)

--- src/Container.js
+++ src/Container.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import Home from './Home.js';
 import About from './About.js';
-import Experience from './Experience.js';
-import Work from './Work.js';
-import Contact from './Contact.js';
+// import Experience from './Experience.js';
+// import Work from './Work.js';
+// import Contact from './Contact.js';
 import { workHistory, jobData } from './utils/workData.js';
+
+const Experience = lazy(() => import('./Experience.js'));
+const Work = lazy(() => import('./Work.js'));
+const Contact = lazy(() => import('./Contact.js'));
+
+const renderLoader = () => <h1>Loading...</h1>
 
 function Container() {
   return (
     <div className="container">
       <Home />
       <About />
-      <Experience workHistory={workHistory} />
-      <Work jobData={jobData} />
-      <Contact />
+      <Suspense fallback={renderLoader()}>
+        <Experience workHistory={workHistory} />
+        <Work jobData={jobData} />
+        <Contact />
+      </Suspense>
     </div>
   )
 }
